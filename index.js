2116614cbe1232f54c2bee96d88409937b19e84c
Matt <matt@rigidassoftware.co.uk>
2018-07-23 22:49:46 +0100

    bus with restart

--- index.js
+++ index.js
@@ -1,40 +1,94 @@
 var service_bus = require('servicebus');
 
-function Bus(options, implOpts) {
-    var bus = service_bus.bus(options, implOpts);
+class Bus {
 
-    bus._subscribe = bus.subscribe;
-    bus._publish = bus.publish;
+    constructor(options, implOpts) {
 
-    bus.subscribe = (queue_name, options) => {
-        bus._subscribe(queue_name, (data, e) => {
-            let key = data.key;
-            if(options.callbacks){
-                let cb = options.callbacks[key];
-                if(cb) {
-                    cb(data.data);
-                }
-                else {
-                    cb = options.callbacks['default'];
-                    if(cb) {
-                        cb(key, data.data);
+        let bus;
+        let on_restart = options.on_restart;
+        
+        let self = this;
+
+        let init = () => {
+
+            bus = service_bus.bus(options, implOpts);
+            bus._subscribe = bus.subscribe;
+            bus._publish = bus.publish;
+
+            bus.subscribe = (queue_name, options) => {
+                bus._subscribe(queue_name, (data, e) => {
+                    let key = data.key;
+                    if (options.callbacks) {
+                        let cb = options.callbacks[key];
+                        if (cb) {
+                            cb(data.data);
+                        }
+                        else {
+                            cb = options.callbacks['default'];
+                            if (cb) {
+                                cb(key, data.data);
+                            }
+                        }
                     }
-                }
+                });
+            };
+
+            bus.publish = (queue, data) => {
+                let queue_parts = queue.split('.');
+                let queue_name = queue_parts[0];
+                let key = queue_parts.length > 1 ? queue_parts[1] : '';
+                bus._publish(queue_name, {
+                    key: key,
+                    data: data
+                });
+            };
+
+        };
+
+        this.start = () => {
+            try {
+                self.init();
+                
+                bus.once('channel.close', event => {
+                    console.error('channel closed');
+                    self.failure(event);
+                });
+
+                console.log('bus connected');
+                return true;
             }
-        });
-    };
+            catch (e) {
+                console.error('error starting bus: ' + e);
+                return false;
+            }
+        };
 
-    bus.publish = (queue, data) => {
-        let queue_parts = queue.split('.');
-        let queue_name = queue_parts[0];
-        let key = queue_parts.length > 1 ? queue_parts[1] : '';
-        bus._publish(queue_name, {
-            key: key,
-            data: data
-        });
-    };
+        this.failure = event => {
+            console.error(event);
+            try {
+                self.try_restart();
+            }
+            catch (e) {
+                console.error(e.stack || e);
+            }
+        };
 
-    return bus;
+        this.tick;
+        this.try_restart = () => {
+            self.tick = setInterval(() => {
+                if (self.start() && self.tick) {
+                    if(on_restart) {
+                        on_restart(bus);
+                    }
+                    clearInterval(self.tick);
+                }
+            }, 10000);
+        };
+
+        this.start();
+
+        return bus;
+    }
 }
 
 module.exports = Bus;
